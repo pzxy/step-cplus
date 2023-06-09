8a7660126f1e7cdbdea5bf300f98d878052ea0a6
kwin <wonkung@163.com>
2023-06-09 21:52:42 +0800

    feat: queue & list

--- targets/06_type_container.md
+++ targets/06_type_container.md
@@ -10,3 +10,28 @@ array是c++ 11新增的序列容器，和其他容器的区别是，array的大�
 
 # vector
 布局和array一样，但是大小可以动态变化。
+![vector](./images/vector.png)
+[demo](./06_type_container_vector.cpp)
+# deque
+双端队列，可以从头尾两端插入或者删除元素。
+![deque](./images/deque.png)
+不支持 data、capacity 和 reserve，因为空间不是连续的。
+如果只从头、尾两个位置对 deque 进行增删操作的话，容器里的对象永远不需要移动。
+由于元素部分连续，所以遍历性很高的。
+如果需要一个经常在头尾增删元素的容器，那 deque 会是个合适的选择。
+
+# list
+插入效率o(1)，随机访问效率o(n)
+list 不提供 data、capacity 和 reserve 成员函数（和 deque 相同）
+![list](./images/list.png)
+排序的话，要使用 list 的成员函数 sort，而不是 std::sort。
+此外还有merge、splice、remove、reverse、unique等成员函数。
+
+# forward_list
+单向链表，只能从头部插入或者删除元素。
+![forward_list](./images/forward_list.png)
+为什么会需要这么一个阉割版的 list 呢？原因是，在元素大小较小的情况下，forward_list 能节约的内存是非常可观的；在列表不长的情况下，不能反向查找也不是个大问题。提高内存利用率，往往就能提高程序性能，更不用说在内存可能不足时的情况了。
+
+# queue & stack
+这两个结构c++中也提供了，就是我们常用的队列和栈。
+他们的push和pop就是算法中的push和pop。
--- targets/06_type_container_vector.cpp
+++ targets/06_type_container_vector.cpp
@@ -19,10 +19,9 @@ void iteratorsVector()
     }
 }
 
-int main()
+void crudVector()
 {
-    iteratorsVector();
-
+    cout << "curdVector" << endl;
     vector<int> vec = {1, 2, 3, 4, 5};
     vec.push_back(6);
     assert(vec.back() == 6);
@@ -42,4 +41,66 @@ int main()
     assert(vec.back() == 33);
     vec.clear();
     assert(vec.size() == 0);
+}
+
+class Obj1
+{
+public:
+    Obj1()
+    {
+        cout << "Obj1()\n";
+    }
+    Obj1(const Obj1 &)
+    {
+        cout << "Obj1(const Obj1&)\n";
+    }
+    Obj1(Obj1 &&)
+    {
+        cout << "Obj1(Obj1&&)\n";
+    }
+};
+
+class Obj2
+{
+public:
+    Obj2()
+    {
+        cout << "Obj2()\n";
+    }
+    Obj2(const Obj2 &)
+    {
+        cout << "Obj2(const Obj2&)\n";
+    }
+    Obj2(Obj2 &&) noexcept
+    {
+        cout << "Obj2(Obj2&&)\n";
+    }
+};
+
+void noexceptVector()
+{
+    // push_back、insert、reserve、resize 会导致内存重新分配。
+    // 如果没有实现移动构造就会调用复制构造函数，影响性能。
+    // 如果要实现了移动构造，那么移动构造要加上 noexcept，否则还是会调用复制构造函数，导致内存重新分配失败。
+    // 需要注意的是push和pop函数适合在尾部进行操作，只有在尾部操作的时候，性能才很高呢。
+    cout << "noexceptVector" << endl;
+    vector<Obj1> v1;
+    v1.reserve(2);
+    cout << "可以看到obj1没有" << endl;
+    v1.emplace_back();
+    v1.emplace_back();
+    v1.emplace_back();
+
+    vector<Obj2> v2;
+    v2.reserve(2);
+    v2.emplace_back();
+    v2.emplace_back();
+    v2.emplace_back();
+}
+
+int main()
+{
+    iteratorsVector();
+    crudVector();
+    noexceptVector();
 }
