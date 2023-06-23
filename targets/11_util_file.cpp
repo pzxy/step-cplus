efce25d9dc3d415a84baa88819ae8536e8759059
kwin <wonkung@163.com>
2023-06-23 17:16:33 +0800

    feat: file operation

--- targets/11_util_file.cpp
+++ targets/11_util_file.cpp
@@ -2,54 +2,6 @@
 #include <fstream>
 using namespace std;
 
-/**
-
-int main ()
-{
-
-   char data[100];
-
-   // 以写模式打开文件
-   ofstream outfile;
-   outfile.open("afile.dat");
-
-   cout << "Writing to the file" << endl;
-   cout << "Enter your name: ";
-   cin.getline(data, 100);
-
-   // 向文件写入用户输入的数据
-   outfile << data << endl;
-
-   cout << "Enter your age: ";
-   cin >> data;
-   cin.ignore();
-
-   // 再次向文件写入用户输入的数据
-   outfile << data << endl;
-
-   // 关闭打开的文件
-   outfile.close();
-
-   // 以读模式打开文件
-   ifstream infile;
-   infile.open("afile.dat");
-
-   cout << "Reading from the file" << endl;
-   infile >> data;
-
-   // 在屏幕上写入数据
-   cout << data << endl;
-
-   // 再次从文件读取数据，并显示它
-   infile >> data;
-   cout << data << endl;
-
-   // 关闭打开的文件
-   infile.close();
-
-   return 0;
-}
-*/
 void open_file()
 {
     // ios::app	追加模式。所有写入都追加到文件末尾。
@@ -67,8 +19,10 @@ void open_file()
 void write_file()
 {
     cout << "> write_file" << endl;
-    ofstream outfile;
+    ofstream outfile; // ofstream只能写入，就算用了 ios::in也只能写入，不能读取。
     outfile.open("outfile", ios::app);
+    outfile << "hello world " << 1 << endl;
+    outfile << "hello world " << 1 << endl;
     outfile << "hello world " << 1 << endl;
 }
 
@@ -77,11 +31,22 @@ void read_file()
     char data[100];
     cout << "> read_file" << endl;
     ifstream infile;
-    infile.open("outfile");
-    infile >> data; // 这种方式每次读取到空格。或者换行。
+    infile.open("outfile"); // ios::out对这 ifstream无效。只能读取，不能写入。
+    infile >> data;         // 这种方式每次读取到空格。或者换行。
     cout << data << endl;
     infile >> data; // 这种方式每次读取到空格。或者换行。
     cout << data << endl;
+    infile >> data; // 这种方式每次读取到空格。或者换行。
+    cout << data << endl;
+    infile.close();
+
+    // 按照行读取。
+    // ifstream f{"outfile"};
+    fstream f{"outfile"};
+    string s;
+    getline(f, s);
+    cout << s << endl;
+    f.close();
 }
 
 int main()
