26e667775a754fd7cf6586536814f47a6689d21b
kwin <wonkung@163.com>
2023-07-12 20:33:21 +0800

    chore: unique shared

--- effective/19_shared_ptr.cpp
+++ effective/19_shared_ptr.cpp
@@ -4,5 +4,6 @@ using namespace std;
 // 2. shared_ptr删除资源通过delete，也可以自定义自己的删除器。
 // 3. 避免从原始指针上创建shared_ptr,因为会导致double-free问题。
 // 4. 当两个类中都有shared_ptr时，要注意循环引用问题。
+// 5. shared_ptr适用于共享资源。
 
 int main() {}
