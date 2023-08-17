56b473582af9c44a0e222d40ed3902ff7175cf6d
kwin <wonkung@163.com>
2023-08-17 09:04:26 +0800

    chore: tidy config

--- crawler/src/app/demo/src/main.cc
+++ crawler/src/app/demo/src/main.cc
@@ -1,7 +1,7 @@
-#include <boost/lambda/lambda.hpp>
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <boost/lambda/lambda.hpp>
 
 int main()
 {
@@ -9,6 +9,6 @@ int main()
     typedef std::istream_iterator<int> in;
 
     std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+        in(std::cin), in(), std::cout << (_1 * 3) << " ");
 }
 // g++ -v -I/opt/homebrew/Cellar/boost/1.82.0_1/include -L/opt/homebrew/Cellar/boost/1.82.0_1/lib file
