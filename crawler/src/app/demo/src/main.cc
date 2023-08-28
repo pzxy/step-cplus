e138a3f0cea7089fd8c81eea30c2d4cddb0ca2ef
kwin <wonkung@163.com>
2023-08-28 14:15:43 +0800

    chore: main.cc

--- crawler/src/app/demo/src/main.cc
+++ crawler/src/app/demo/src/main.cc
@@ -2,13 +2,16 @@
 #include <iterator>
 #include <algorithm>
 #include <boost/lambda/lambda.hpp>
+#include <string>
+using namespace std;
 
-int main()
-{
+int main() {
     using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
-
+    string s = R"(asd)";
+    cout << s << endl;
     std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " ");
+            in(std::cin), in(), std::cout << (_1 * 3) << " ");
+
 }
 // g++ -v -I/opt/homebrew/Cellar/boost/1.82.0_1/include -L/opt/homebrew/Cellar/boost/1.82.0_1/lib file
