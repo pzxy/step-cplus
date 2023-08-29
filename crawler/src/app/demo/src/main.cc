6ceb8ea384b3ac09ee40c4642c6844c645611086
kwin <wonkung@163.com>
2023-08-29 22:15:53 +0800

    chore: cmake

--- crawler/src/app/demo/src/main.cc
+++ crawler/src/app/demo/src/main.cc
@@ -1,8 +1,7 @@
-#include <httplib/httplib.h>
+#include <httplib.h>
 #include <iostream>
 
 using namespace std;
-
 int main() {
     httplib::Client cli("https://time.geekbang.org");
     auto res = cli.Get("/resource?m=0&d=3&c=3");
