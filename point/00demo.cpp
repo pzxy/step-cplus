028edb59d870aa8a92be2afad2405a53eaa8a912
kwin <wonkung@163.com>
2024-04-05 22:02:36 +0800

    chore: point

--- point/00demo.cpp
+++ point/00demo.cpp
@@ -19,21 +19,22 @@ int main()
     int *pt = new int;
     const int b = 101;
     // 这里获取n的引用,要注意.
-    // 这里的&表示引用,可以将int& 是一个整体,通用表示的话是 T&
-    int &rn = n;
+    // &可以取地址，也可以表示引用类型。
+    // 如果&表示类型的话，应该当成整体看待，也就是说int& 是一个类型,通用表示的话是 T&
+    int &rn = n;// 此时 rn 和 n是一个东西。
     // 这里的*是一个解引用操作符,会访问地址指向空间的值.
     // 上面提到了 pt表示一个地址,所以这里的*pt就是获取pt指向的值
     int &rt = *pt;
     // 这里也可以将 const int & 看成一个整体.通用表示的话是 const T&
     // 因为b是const int类型,要获取b的引用,规定只能是const T& 类型.
     const int &rb = b;
-    cout << n << endl;
-    cout << rt << endl;
-    cout << *&rt << endl;
+    cout << n << endl;// n是1，实际上是个任意的值。
+    cout << rn << endl;//1 因为rn是n的引用。
+    cout << *&rn << endl;// 1，&获取rn取地址，*再访问rn的地址，读出来还是n的值。
 
     A a;
     a.a = 4;
-    A & b = a;
-    fa(b);
+    A & aa = a;
+    fa(aa);
     cout << a.a << endl;
 }
